de79e1a3e0a21f2703abdb6a8f3d296632f46fa3
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2021-05-25 01:36:24 -0400

    Let y Const, Multilinea, Spread Operator y Destructuracion

--- src/es6/index.js
+++ src/es6/index.js
@@ -25,6 +25,36 @@ let epicPhrase = hello + " " + world;
 console.log(epicPhrase);
 
 //es6
+console.log(`${hello} ${world}`);
 let dorime = "dorime";
 let ameno = "ameno";
 console.log(`${dorime} ${ameno}`);
+
+//Let y Const, Multilinea, Spread Operator y Destructuracion
+//Multilinea
+let lorem = "Lroemdaom dasda \n" + "Frase epica";
+
+//es6
+let lorem2 = `Òtra frase app.put('route', handler);
+para practicar es6`;
+
+//Destructuracion
+let person = {
+  name: "Luis",
+  age: 22,
+  country: "Vzl",
+};
+console.log(person.name, person.age, person.country);
+
+//es6
+let { name, age, country } = person;
+console.log(name, age, country);
+
+//Spread Operator
+let team1 = ["oscar", "julian", "ricardo"];
+let team2 = ["valeria", "yesica", "camila"];
+
+//es6
+let eduaction = ["David", ...team1, ...team2];
+
+console.log(eduaction);
